ddbc7cd6a9c39d990af3ff8531b5f77686e8bf8f
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2010-12-10 14:24:38 -0500

    Fix quotes.

--- flaskext/virtualenv.py
+++ flaskext/virtualenv.py
@@ -15,7 +15,7 @@ Flask-Script
 
 def _local(cmd, cwd=os.getcwd()):
     """Simple helper for executing shell."""
-    print "Execute: `%s` from `%s`" % (cmd, cwd)
+    print 'Execute: `%s` from `%s`' % (cmd, cwd)
     p = subprocess.Popen(cmd, shell=True, cwd=cwd, stderr=subprocess.STDOUT)
     if (p.wait() != 0):
         raise Exception()
@@ -28,7 +28,7 @@ def _ensure_requirements_file():
         fp = open('requirements.txt', 'w')
         fp.write(DEFAULT_REQUIREMENTS)
         fp.close()
-        print "Created default requirements file."
+        print 'Created default requirements file.'
 
 
 def install_commands(manager):
@@ -40,13 +40,13 @@ def install_commands(manager):
     def virtualenvrequirements():
         """Print required packages and versions"""
         _ensure_requirements_file()
-        _local("cat requirements.txt")
+        _local('cat requirements.txt')
 
     @manager.command
     def createvirtualenvrequirements():
         """Generate new requirements file"""
         _ensure_requirements_file()
-        _local("./%s/bin/pip freeze > requirements.txt" % _env_name)
+        _local('./%s/bin/pip freeze > requirements.txt' % _env_name)
 
     @manager.command
     def createvirtualenv():
@@ -54,13 +54,13 @@ def install_commands(manager):
         _ensure_requirements_file()
 
         if not os.path.exists('_env_name'):
-            _local("virtualenv --no-site-packages --distribute --prompt=%s> %s"
+            _local('virtualenv --no-site-packages --distribute --prompt="%s>" %s'
                 % (_app_name, _env_name))
 
         if not os.path.exists('./%s/bin/pip' % _env_name):
-            _local("./%s/bin/easy_install pip" % (_env_name))
+            _local('./%s/bin/easy_install pip' % (_env_name))
 
-        _local("./%s/bin/pip install -r requirements.txt" % _env_name)
+        _local('./%s/bin/pip install -r requirements.txt' % _env_name)
 
         # Add env to .gitignore
         if os.path.exists('.gitignore'):
@@ -68,14 +68,14 @@ def install_commands(manager):
             contents = fp.read()
             if not _env_name in contents:
                 fp.write('## Ignore virtual environment\n%s/\n' % (_env_name))
-                print "Added %s/ to your .gitignore" % _env_name
+                print 'Added %s/ to your .gitignore' % _env_name
             fp.close()
         activatevirtualenv()
 
     @manager.command
     def destroyvirtualenv():
         """Clear the current virtual environment."""
-        _local("virtualenv --clear %s")
+        _local('virtualenv --clear %s')
 
     @manager.command
     def activatevirtualenv():
